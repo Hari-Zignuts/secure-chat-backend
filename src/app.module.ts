c0457827ae880fc7fb56df3b1b0580576324bca0
Hari Malam <harim@zignuts.com>
2025-02-13 12:44:28 +0530

    initial commit

---
+++ src/app.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { ChatModule } from './chat/chat.module';
+import { FilesModule } from './files/files.module';
+import { ConfigModule } from './config/config.module';
+import { RedisModule } from './redis/redis.module';
+import { DatabaseModule } from './database/database.module';
+import { EncryptionModule } from './encryption/encryption.module';
+
+@Module({
+  imports: [AuthModule, UsersModule, ChatModule, FilesModule, ConfigModule, RedisModule, DatabaseModule, EncryptionModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
